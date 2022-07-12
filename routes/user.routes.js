2e29c5651d40cf12e6b7c671e567eab2d9ba792f
Flavia <flavia.hotts@gmail.com>
2022-07-12 21:06:45 +0100

    read done 2

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -14,4 +14,12 @@ router.get("/read", (req, res) => {
 res.status(200).json({ data: data });
 });
 
+router.get("/details/:id", (req, res) => {
+  const { id } = req.params;
+
+  const document = data.filter((currentDocument) => currentDocument.id === id);
+
+  return res.status(200).json(document[0]);
+});
+
 module.exports = router;
