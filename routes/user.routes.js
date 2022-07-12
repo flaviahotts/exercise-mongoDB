d8392688dc7912d7885911d5ad6b2a737d4d0dd2
Flavia <flavia.hotts@gmail.com>
2022-07-12 21:22:26 +0100

    edit done

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -16,10 +16,24 @@ res.status(200).json({ data: data });
 
 router.get("/details/:id", (req, res) => {
   const { id } = req.params;
-
   const document = data.filter((currentDocument) => currentDocument.id === id);
-
   return res.status(200).json(document[0]);
+});
+
+router.put("/edit/:id", (req, res) => {
+  const { id } = req.params;
+
+  data.forEach((currentDocument, i) => {
+    if (currentDocument.id === id) {
+      data[i] = { ...req.body, id: currentDocument.id };
+    }
+  });
+
+  const newDocument = data.filter(
+    (currentDocument) => currentDocument.id === id
+  );
+
+  return res.status(200).json(newDocument[0]);
 });
 
 module.exports = router;
